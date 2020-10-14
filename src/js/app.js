ffd0a372e365aeb35d928d81fae10af8e423abfb
OlegBirykov <p_petro75@mail.ru>
2020-10-15 00:02:09 +0300

    Задание выполнено

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,8 @@
-// eslint-disable-next-line no-console
-console.log('it works!');
+import ImageManager from './ImageManager';
+
+const manager = new ImageManager(
+  document.querySelector('.form-container'),
+  document.querySelector('.images-container'),
+);
+
+manager.bindToDOM();
